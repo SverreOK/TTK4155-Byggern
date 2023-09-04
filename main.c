a28833b07292db02e089f7607f2f5197babb91f4
JonnyErDigg <yu@exe.com>
2023-09-04 11:01:05 +0200

    Blinking led

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
 //test
+#define F_CPU 16000000
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+    DDRA |= (1 << PA0);
+
+    while (1) {
+        PORTA |= (1 << PA0);
+        _delay_ms(10);
+        PORTA &= ~(1 << PA0);
+        _delay_ms(10);
+    }
+
+    return 0;
+}
