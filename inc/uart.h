20d92bf9eea0005cf438a1fe280ef36eb06c6e54
SverreOK <sverre.o.k@hotmail.no>
2023-09-04 11:33:33 +0200

    some more stuff

--- inc/uart.h
+++ inc/uart.h
@@ -2,13 +2,19 @@
 
 // SOURCE: DATASHEET
 
+// INCLUDES
 #include <avr/io.h>
 #include <utility/delay.h>
 #include <stdlib.h>
 
-#define BAUD 9600
-#define FOSC 4913200
-#define MYUBRR FOSC/16/BAUD-1
+
+// DEFINES
+#define BAUD 9600 // baud rate
+#define FOSC 4913200 // some clock stuff? is it F_CPU?
+#define MYUBRR FOSC/16/BAUD-1 // uart baud rate register
+
+// FUNCTIONS
+void uart_init();
 
 void uart_transmit(char data);
 
