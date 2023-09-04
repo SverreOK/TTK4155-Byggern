67ee513e4baf1d2b6c80d243cacd558636091bd7
SverreOK <sverre.o.k@hotmail.no>
2023-09-04 11:28:32 +0200

    uart header file

--- inc/uart.h
+++ inc/uart.h
@@ -1,1 +1,15 @@
 //uart header
+
+// SOURCE: DATASHEET
+
+#include <avr/io.h>
+#include <utility/delay.h>
+#include <stdlib.h>
+
+#define BAUD 9600
+#define FOSC 4913200
+#define MYUBRR FOSC/16/BAUD-1
+
+void uart_transmit(char data);
+
+void uart_receive();
