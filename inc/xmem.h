9aca52bed929cda21e0ad19556c96f16f99e8b43
JonnyErDigg <yu@exe.com>
2023-09-05 14:43:25 +0200

    SRAM working

---
+++ inc/xmem.h
@@ -0,0 +1,2 @@
+
+void xmem_init();
---
+++ src/xmem.c
@@ -0,0 +1,10 @@
+#include "../inc/xmem.h"
+#include <avr/io.h>
+
+void xmem_init() {
+    MCUCR |= (1<<SRE); // xmem enable
+
+    SFIOR |= (1<<XMM2);
+    SFIOR &= ~(1<<XMM1);
+    SFIOR &= ~(1<<XMM0);
+}
